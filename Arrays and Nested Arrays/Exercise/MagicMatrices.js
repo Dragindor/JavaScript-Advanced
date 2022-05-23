281be84275c35995a76e7923bdb20641006cc549
Dragindor <86678395+Dragindor@users.noreply.github.com>
2022-05-23 20:09:53 +0300

    ninth problem

---
+++ Arrays and Nested Arrays/Exercise/MagicMatrices.js
@@ -0,0 +1,28 @@
+function strlen(matrix)
+{
+    let isMagical = true;
+
+    for (let index = 0; index < matrix.length - 1; index++) 
+    {
+        let sumRowOne = matrix[index].reduce((a,b) => a + b, 0);
+        let sumRowTwo = matrix[index + 1].reduce((a,b) => a + b, 0);
+        let sumColOne = 0;
+        let sumColTwo = 0;
+
+        for (let j = 0; j < matrix.length; j++) {
+            sumColOne += matrix[index][j];
+            sumColTwo += matrix[index + 1][j];            
+        }
+
+        if (sumRowOne !== sumRowTwo || sumColOne !== sumColTwo) 
+        {
+            isMagical=false;
+        }
+    }
+
+    return isMagical;
+}
+
+strlen([[4, 5, 6],
+    [6, 5, 4],  
+    [5, 5, 5]])
