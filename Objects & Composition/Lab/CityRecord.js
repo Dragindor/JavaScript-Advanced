fe99e13e1bcb34b3fdd8ae69a6f68700fad9fb6d
Dragindor <86678395+Dragindor@users.noreply.github.com>
2022-05-25 18:50:56 +0300

    first problem

---
+++ Objects & Composition/Lab/CityRecord.js
@@ -0,0 +1,15 @@
+function strlen(cityName, cityPopulation, cityTreasury) 
+{
+    const city=
+    {
+        name: cityName,
+        population: cityPopulation,
+        treasury: cityTreasury
+    };
+
+    return city;
+}
+
+strlen('Tortuga',
+7000,
+15000);
