3bded1fc5094d82a95ab79c7d677d570449d676e
Dragindor <86678395+Dragindor@users.noreply.github.com>
2022-06-06 18:49:51 +0300

    finished

--- Advanced Functions/Exercise/02. ArgumentInfo.js
+++ Advanced Functions/Exercise/02. ArgumentInfo.js
@@ -1,6 +1,19 @@
-function argInfo(Info)
+function argInfo()
 {
-    
+    const params = Array.from(arguments);
+    const types = {};
+
+    for (const arg of params) {
+        const type = typeof arg;
+        console.log(`${type}: ${arg}`);
+
+        if (types[type] == undefined) {
+            types[type] = 0;
+        }
+        types[type]++;
+    }
+
+    console.log(Object.entries(types).sort((a, b) => b[1] - a[1]));
 }
 
-argInfo('cat', 42, function () { console.log('Hello world!'); })
+argInfo('cat', 42, function () { console.log('Hello world!'); });
